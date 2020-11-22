b06a782a7dba5a70bda191f1e9e54b963fb44002
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-22 03:03:32 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -16,5 +16,5 @@ def find_even_values(array_of_array)
   end
   
 end
-
-find_even_values( [ [10, 11], [99, 50, 3, 4], [23, 41] ])
+array = [ [10, 11], [99, 50, 3, 4], [23, 41] ] 
+find_even_values(array)
